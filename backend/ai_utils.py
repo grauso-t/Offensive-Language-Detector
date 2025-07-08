ce0563379863062bef826f9e169a296bfa5e62e9
grauso-t <grausoteodoro@gmail.com>
2025-07-08 13:14:09 +0200

    Translator implementation

--- backend/ai_utils.py
+++ backend/ai_utils.py
@@ -1,23 +1,49 @@
 from transformers import pipeline
+from googletrans import Translator
+import langdetect
 
-# Translation Pipeline
-translator = pipeline("translation", model="Helsinki-NLP/opus-mt-mul-en")
+# Tranlator
+translator = Translator()
 
 # Toxicity Classification
 classifier = pipeline("text-classification", model="unitary/toxic-bert")
+
+def translate_to_english(text):
+    """
+    Translate the provided text to English if it is not already in English.
+    """
+    if not text.strip():
+        return None
+    
+    try:
+        detected_lang = langdetect.detect(text)
+    except:
+        return None
+    
+    if detected_lang == 'en':
+        return text
+    
+    try:
+        result = translator.translate(text, dest='en')
+        return result.text
+    except Exception as e:
+        return None
 
 def is_offensive(text):
     """
     Check if the provided text is offensive by translating it to English
     """
-    translated = translator(text, max_length=512)[0]['translation_text']
-    print(f"Translated text: {translated}")
-    result = classifier(translated)
+    translation = translate_to_english(text)
 
-    offensive_labels = ["toxic", "severe_toxic", "obscene", "threat", "insult", "identity_hate"]
-    for item in result:
-        label = item['label'].lower()
-        score = item['score']
-        if label in offensive_labels and score > 0.5:
-            return True
-    return False
+    if text is not None:
+        result = classifier(translation)
+
+        offensive_labels = ["toxic", "severe_toxic", "obscene", "threat", "insult", "identity_hate"]
+        for item in result:
+            label = item['label'].lower()
+            score = item['score']
+            if label in offensive_labels and score > 0.5:
+                return True
+        return False
+    else:
+        return None
